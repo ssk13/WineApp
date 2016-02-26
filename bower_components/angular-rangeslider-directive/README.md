e7c98b36dc338766d75b095c8bb39c81eb0891c3
Sarah Klein <Ssk13@ufl.du>
2016-02-25 22:03:26 -0500

    started app updated readme

---
+++ bower_components/angular-rangeslider-directive/README.md
@@ -0,0 +1,52 @@
+angular-rangeslider-directive
+==============
+
+Styleable Range Slider Control directive for Angular with no jQuery or browser dependencies.
+Does not use the input range type. Allows for binding to model data.
+
+## Usage
+
+    angular
+        .module('app', ['angularRangeSlider'])
+        .controller('AppController', function($scope) {
+            $scope.sliderValue = 200;
+            $scope.lowerValue = 400;
+            $scope.upperValue = 600;
+        });
+
+### Single Item Example:
+
+    <p>Value: {{sliderValue}}</p>
+    <div range-slider
+         floor="100"
+         ceiling="1000"
+         step="50"
+         precision="2"
+         highlight="left"
+         ng-model="sliderValue"></div>
+
+### Range:
+
+    <p>Lower Value: {{lowerValue}}</p>
+    <p>Value 2: {{upperValue}}</p>
+    <div range-slider
+         floor="100"
+         ceiling="1000"
+         dragstop="true"
+         ng-model-low="lowerValue"
+         ng-model-high="upperValue"></div>
+
+## Properties:
+
+* __floor__ `{number}` Minimum Value for Slider
+* __ceiling__ `{number}` Maximum Value for Slider
+* __step__ `{number}` Value between steps in snapping on the scale
+* __highlight__ `{string}` Left, Right, or false for single handle selectors, true or false for range selectors
+* __precision__ `{number}` Maximum Value for Slider
+* __dragstop__ `{boolean}` True will not update bound model data until mouse up
+* __ngModel__ `{number}` Bound value for a single handle slider
+* __ngModelLow__ `{number}` Bound value for lower limit of range selection
+* __ngModelHigh__ `{number}` Bound value for upper limit of range selection
+
+
+### License: MIT
---
+++ js/script.js
@@ -0,0 +1,237 @@
+// create the module and name it wineApp
+var wineApp = angular.module('wineApp', []);
+
+var wineApp = angular.module('wineApp', ['ngRoute', 'angularRangeSlider']);
+
+// configure our routes
+wineApp.config(function($routeProvider) {
+    $routeProvider
+
+        // route for the home page
+        .when('/', {
+            templateUrl : 'pages/site-components/home.html',
+            controller  : 'mainController'
+        })
+
+        // route for the home page
+        .when('/home', {
+            templateUrl : 'pages/site-components/home.html',
+            controller  : 'mainController'
+        })
+
+        // route for the about page
+        .when('/login', {
+            templateUrl : 'pages/account/login.html',
+            controller  : 'loginController'
+        })
+
+        // route for the sign-up page
+        .when('/signup', {
+            templateUrl : 'pages/account/signup.html',
+            controller  : 'signupController'
+        })
+
+        // route for the log-in page
+        .when('/learn', {
+            templateUrl : 'pages/learn/learn.html',
+            controller  : 'learnController'
+        })
+
+        // route for the quiz
+        .when('/quiz', {
+            templateUrl : 'pages/quiz/quiz.html',
+            controller  : 'quizController'
+        })
+
+        // route for the results
+        .when('/results', {
+            templateUrl : 'pages/results/results.html',
+            controller  : 'resultsController'
+        })
+
+        // route for the quiz
+        .when('/saved', {
+            templateUrl : 'pages/user/saved.html',
+            controller  : 'savedController'
+        })
+
+        // route for the results
+        .when('/taste', {
+            templateUrl : 'pages/user/taste-profile.html',
+            controller  : 'tasteController'
+        })
+});
+
+wineApp.controller('mainController', ['$scope', '$rootScope', '$location', 
+    function ($scope, $rootScope, $location) {
+        // Expose view variables
+        $rootScope.isLoggedIn = $rootScope.isLoggedIn == true || false;
+
+        $scope.logIn = function() {
+            $rootScope.isLoggedIn = true;
+            $location.path("/home");
+
+        };
+
+        $scope.logOut = function() {
+            $rootScope.isLoggedIn = false;
+            $location.path("/home");
+        };
+    }
+]);
+
+// create the controller and inject Angular's $scope
+wineApp.controller('loginController', function($scope) {
+
+});
+
+// create the controller and inject Angular's $scope
+wineApp.controller('signupController', function($scope) {
+
+});
+
+// create the controller and inject Angular's $scope
+wineApp.controller('learnController', function($scope) {
+
+});
+
+// create the controller and inject Angular's $scope
+wineApp.controller('quizController', function($scope, $location) {
+    $scope.progress = 0;
+    $scope.currentQuestion = 0;
+    $scope.totalQuestions = 11;
+    $scope.formData = {
+        priceRange: {
+            low: 5,
+            high: 200
+        },
+        hasColorPreference: {
+            value: 'true'
+        },
+        colorPreference: {
+            red: {
+                low: 0,
+                high: 10
+            },
+            white: {
+                low: 0,
+                high: 10
+            }
+        },
+        hasWineTypePreference: {
+            value: 'true'
+        },
+        wineTypePreference: {
+            merlot: 'false',
+            zinfandel: 'false',
+            pinotNoir: 'false'
+        },
+        hasWherePreference: {
+            value: 'true'
+        },
+        wherePreference: {
+            argentina: 'false',
+            california: 'false'
+        },
+        isForMeal: {
+            value: 'yes'
+        },
+        iceCreamPreference: {
+            chocolate: 'false',
+            strawberry: 'false',
+            fig: 'false'
+        },
+        sweetenPreference: {
+            low: 0,
+            high: 10
+        },
+        smells: {
+            fire: 'false',
+            flowers: 'false',
+            beach: 'false'
+        },
+        mealType: {
+            value: 'dinner'
+        },
+        dinnerType: {
+            chicken: 'false',
+            fish: 'false',
+            vegetables: 'false'
+        },
+        dessertType: {
+            cake: 'false',
+            fruit: 'false'
+        }
+    }
+    $scope.sliderValue = 20;
+    $scope.lowerValue = 5;
+    $scope.upperValue = 200;
+    $scope.forMeal;
+
+    $scope.pagerClicked = function(value) {
+        if (value == -1) {
+            if ($scope.currentQuestion == 4) {
+                if ($scope.formData.hasWineTypePreference.value == 'false') {
+                    if ($scope.formData.hasColorPreference.value == 'false') {
+                        if ($scope.formData.hasWherePreference.value == 'false') {
+                            $scope.currentQuestion = 1;
+                            return;
+                        } else {
+                            $scope.currentQuestion = 2;
+                            return;
+                        }
+                    } else {
+                        $scope.currentQuestion = 2;
+                        return;
+                    }    
+                }
+            } else if ($scope.currentQuestion == 8) {
+                $scope.currentQuestion = 4;
+                return;
+            }
+
+            $scope.currentQuestion += value;
+            return;
+        }
+
+        if ($scope.currentQuestion == 1) {
+            if ($scope.formData.hasColorPreference.value == 'false') {
+                $scope.formData.hasWineTypePreference.value = 'false';
+                $scope.formData.hasWherePreference.value = 'false';
+                $scope.currentQuestion = 4;
+                return;
+            }
+        } else if ($scope.currentQuestion == 2) {
+            if ($scope.formData.hasWineTypePreference.value == 'false') {
+                $scope.formData.hasWherePreference.value = 'false';
+                $scope.currentQuestion = 4;
+                return;
+            }
+        } else if ($scope.currentQuestion == 4) {
+            if ($scope.formData.isForMeal.value == 'yes') {
+                $scope.currentQuestion = 8;
+                return;
+            }
+        }
+        
+        $scope.currentQuestion += value;
+        return;
+    };
+
+    $scope.submitQuiz = function(value) {
+        $location.path( "/results" );
+    };
+});
+
+// create the controller and inject Angular's $scope
+wineApp.controller('resultsController', function($scope) {
+});
+
+// create the controller and inject Angular's $scope
+wineApp.controller('savedController', function($scope) {
+
+});
+
+// create the controller and inject Angular's $scope
+wineApp.controller('tasteController', function($scope) {
+});
